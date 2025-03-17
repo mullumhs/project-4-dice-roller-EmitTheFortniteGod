660a8ebd23ca19905d13ba22ed53bc6c51bd307f
EmitTheFortniteGod <147779921+EmitTheFortniteGod@users.noreply.github.com>
2025-03-18 09:49:24 +1100

    Update dice-roller.py

--- dice-roller.py
+++ dice-roller.py
@@ -1,8 +1,42 @@
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-                                GUESSING GAME
+                                DICE GAME
 ---------------------------------------------------------------------------------
 - File Name: dice-roller.py
 - Teacher: David Steedman
 - Class: Software Engineering
 - Description: Complete a functional dice roller app in Python
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+print("*"*50)
+print(" \n \n \n")
+print("-"* 18, "Dice roller", "-"* 19)
+print(" \n \n \n")
+print("*"*50)
+
+
+import random
+
+#function 4 dice
+
+
+def roll(qty, sides):
+    for i in range(qty):
+        roll = random.randint (sides)
+        return roll
+
+
+qty = int(input(print("how many dice do u want to roll: ")))
+sides = int (input(print("how many sides would u like this dice to have: ")))
+
+
+
+roll(qty, sides)
+
+
+
+
+
+
+ 
+
+
+
